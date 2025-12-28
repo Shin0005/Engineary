64d84e80d0e50620ad3498ed7a5f8199b4e512e8
Shin0005 <kuroppen123@gmail.com>
2025-12-28 15:49:43 +0900

    create springboot pj

---
+++ src/main/java/com/example/engineary/EnginearyApplication.java
@@ -0,0 +1,13 @@
+package com.example.engineary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnginearyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnginearyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/engineary/EnginearyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.engineary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnginearyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
