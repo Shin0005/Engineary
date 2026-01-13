529b643c86cccdff6ec7b345137b37e071d6fb95
Shin0005 <kuroppen123@gmail.com>
2026-01-14 00:24:11 +0900

    DiaryEntryのCRUDの内、CRUを追加

--- src/main/java/com/example/engineary/controller/DiaryEntryController.java
+++ src/main/java/com/example/engineary/controller/DiaryEntryController.java
@@ -4,22 +4,55 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import com.example.engineary.model.DiaryEntry;
+import com.example.engineary.service.DiaryEntryService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @RestController
 @RequestMapping("/diary")
 public class DiaryEntryController {
 
+    private final DiaryEntryService diaryEntryService;
+
+    public DiaryEntryController(DiaryEntryService diaryEntryService) {
+        this.diaryEntryService = diaryEntryService;
+    }
+
+    //CRUD作成
     //全投稿取得
     @GetMapping("/entries")
     public ResponseEntity<List<DiaryEntry>> getAllEntries(){
-        List<DiaryEntry> entries = null;//diaryEntryService.getAllEntries();
-
-
+        List<DiaryEntry> entries = diaryEntryService.getAllEntries();
         return ResponseEntity.ok(entries);
     }
 
+    //create
+    @PostMapping
+    public ResponseEntity<DiaryEntry> createDiaryEntry(@RequestBody DiaryEntry diaryEntry){
+        DiaryEntry entry = diaryEntryService.createDiaryEntry(diaryEntry);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(entry);
+    }
+
+    //update
+    @PostMapping("/{id}")
+    public ResponseEntity<DiaryEntry> updateDiaryEntry(
+        @PathVariable Long id,
+        @RequestBody DiaryEntry entryDetails) {
+
+        //update実行
+        try{
+            DiaryEntry updatedEntry = diaryEntryService.updateDiaryEntry(id, entryDetails);
+            return ResponseEntity.ok(updatedEntry);
+        }catch(RuntimeException e){
+            //eをどう使う。
+            return ResponseEntity.notFound().build();
+        }
+    }
     
 }
--- src/main/java/com/example/engineary/service/DiaryEntryService.java
+++ src/main/java/com/example/engineary/service/DiaryEntryService.java
@@ -11,15 +11,28 @@ import com.example.engineary.repository.DiaryEntryRepository;
 @Service
 public class DiaryEntryService {
 
-    /* レポジトリーフィールド */
     private final DiaryEntryRepository diaryEntryRepository;
 
-    /* レポジトリーコンストラクタ */
     public DiaryEntryService(DiaryEntryRepository diaryEntryRepository){
         this.diaryEntryRepository = diaryEntryRepository;
     }
 
+    //selectAll
     public List<DiaryEntry> getAllEntries(){
         return diaryEntryRepository.findAll();
+    }
+
+    //create
+    public DiaryEntry createDiaryEntry(DiaryEntry diaryEntry){
+        return diaryEntryRepository.save(diaryEntry);
+    }
+
+    //update
+    public DiaryEntry updateDiaryEntry(Long id, DiaryEntry entryDetails){
+        //Optional<DEntry>をDEntryに変換できないと出るが、optionalはnullかもしれないことを
+        //表すので、nullの例外処理を書かないと型変換できない?ggr
+        DiaryEntry entry = diaryEntryRepository.findById(id);
+        entry.setTitle(entryDetails.getTitle());
+        //項目追加・lombok用の書き方を調べる
     }
 }
