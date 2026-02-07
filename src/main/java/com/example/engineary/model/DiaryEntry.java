e0136bbabc078dbf5d5a1eb31485f64f130389ee
Shin0005 <kuroppen123@gmail.com>
2026-02-07 17:21:39 +0900

    diaryモデルにバリデーションと更新日時処理を追加
    タイトルに NotBlank を追加
    
    各フィールドに Javadoc を追加
    
    最終更新日時の自動更新 (@UpdateTimestamp) を実装

--- src/main/java/com/example/engineary/model/DiaryEntry.java
+++ src/main/java/com/example/engineary/model/DiaryEntry.java
@@ -3,10 +3,12 @@ package com.example.engineary.model;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 /**
@@ -17,32 +19,33 @@ import lombok.Data;
 @Data
 @Table(name = "entries")
 public class DiaryEntry {
-    /* 投稿Id*/
+    /** 投稿Id*/
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    /* タイトル*/
+    /** タイトル*/
+    @NotBlank
     private String title;
 
-    /* tag 別テーブルから参照*/
+    /** tag 別テーブルから参照*/
     //private Set<Tag> tag;
 
-    /* 内容 */
+    /** 内容 */
     private String contents;
 
-    /* 作業時間 */
+    /** 作業時間 */
     private Double workedTime;
 
-    /* 作業日時 デフォルトで当日*/
-    @CreatedDate
+    /** 作業日時 */
     private LocalDate workedDate;
 
-    /* 最終更新日時 */
+    /** 最終更新日時 */
+    @UpdateTimestamp
     @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
-    /* 作成日時 */
+    /** 作成日時 */
     @CreatedDate
     @Column(name = "created_at", nullable = false, updatable = false)
     private LocalDateTime createdAt;
