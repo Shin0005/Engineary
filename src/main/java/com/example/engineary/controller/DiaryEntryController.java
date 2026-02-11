4d686ddc0e59431f13f8ce3f8775e7aca4df06c5
Shin0005 <kuroppen123@gmail.com>
2026-02-11 16:15:24 +0900

    DTOフォルダ名をdtoに変更。余計なimportを削除

--- src/main/java/com/example/engineary/controller/DiaryEntryController.java
+++ src/main/java/com/example/engineary/controller/DiaryEntryController.java
@@ -7,10 +7,8 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import com.example.engineary.DTO.DiaryEntryRequest;
-import com.example.engineary.DTO.DiaryEntryResponse;
-import com.example.engineary.mapper.DiaryEntryMappar;
-import com.example.engineary.model.DiaryEntry;
+import com.example.engineary.dto.DiaryEntryRequest;
+import com.example.engineary.dto.DiaryEntryResponse;
 import com.example.engineary.service.DiaryEntryService;
 
 import jakarta.validation.Valid;
