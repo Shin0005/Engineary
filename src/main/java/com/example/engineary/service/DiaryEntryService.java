af1c1f44862fd410751465ba35e0ed7e328c9f2a
Shin0005 <kuroppen123@gmail.com>
2026-02-28 18:03:52 +0900

    javadocコメントを追加

--- src/main/java/com/example/engineary/service/DiaryEntryService.java
+++ src/main/java/com/example/engineary/service/DiaryEntryService.java
@@ -23,7 +23,14 @@ public class DiaryEntryService {
         this.diaryEntryRepository = diaryEntryRepository;
     }
 
-    // selectAll
+    /**
+     * ページングを用いてレポジトリから日誌を取得する<br>
+     * 1. repositoryから日誌を取得<br>
+     * 2. DiaryEntryからResponse変換して返却
+     * 
+     * @param pageable
+     * @return Page&lt;DiaryEntryResponse&gt; 複数の日誌
+     */
     @Transactional
     public Page<DiaryEntryResponse> getAllEntries(Pageable pageable) {
         // ページング 10個ごと
@@ -33,7 +40,15 @@ public class DiaryEntryService {
         return responses;
     }
 
-    // create
+    /**
+     * リクエストを受け取り日誌を新規作成<br>
+     * 1. requestをEntityに変換<br>
+     * 2. DBに保存・結果を取得<br>
+     * 3. 結果をresponseにして返却
+     * 
+     * @param request DiaryEntryRequest
+     * @return DiaryEntryResponse 保存した日誌
+     */
     public DiaryEntryResponse createDiaryEntry(DiaryEntryRequest request) {
 
         DiaryEntry inputEntity = DiaryEntryMapper.toEntity(request);
@@ -44,7 +59,15 @@ public class DiaryEntryService {
         return response;
     }
 
-    // update
+    /**
+     * id指定されたリクエストを受け取り日誌を更新<br>
+     * 1. 指定されたidの日誌を検索し、日誌を返却<br>
+     * 2. 日誌内容をrequestに代入<br>
+     * 3. 日誌をDBに保存
+     * 
+     * @param id      Long
+     * @param request DiaryEntryRequest
+     */
     public void updateDiaryEntry(Long id, DiaryEntryRequest request) {
         DiaryEntry entry = diaryEntryRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException(id));
@@ -58,7 +81,13 @@ public class DiaryEntryService {
         diaryEntryRepository.save(entry);
     }
 
-    // delete
+    /**
+     * 指定されたidの日誌を削除<br>
+     * 1. 指定されたidの日誌を検索し、日誌を返却
+     * 2. DBから日誌を削除
+     * 
+     * @param id Long
+     */
     public void deleteDiaryEntry(Long id) {
 
         // エラーを明確に出すためにfind->delete
