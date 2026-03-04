ed4ab52ba1b95c4cdc815e4878e2bb2d549fc749
Shin0005 <kuroppen123@gmail.com>
2026-03-04 20:41:55 +0900

    ルーティング機能の追加
    urlをフラグとして機能を変えるための機能
    ブラウザの戻る進む機能を使うためにstateによりurlの状態を管理
    
    URL変更をトリガーとしてapp.jsの画面描画関数を実行するイベントを登録。

---
+++ src/main/resources/static/js/router.js
@@ -0,0 +1,24 @@
+/**
+ * ルーター初期化
+ * URL state の管理のみ行う。何を描画するかはコールバックに委譲。
+ * @param {Function} initWindow ウィンドウ初期化関数
+ */
+export const initRouter = (initWindow) => {
+
+    // リンク初期化（リンククリック → URLを特定）
+    document.body.addEventListener("click", (e) => {
+        if (!e.target.matches("[data-link]")) return;
+        e.preventDefault();
+
+        // URLを切り替え（未発火）
+        window.history.pushState(null, null, e.target.href);
+
+        // コールバック関数を実行
+        initWindow(window.location.pathname);
+    });
+
+    // ブラウザの戻る・進むボタン対応
+    window.addEventListener("popstate", () => {
+        initWindow(window.location.pathname);
+    });
+};
